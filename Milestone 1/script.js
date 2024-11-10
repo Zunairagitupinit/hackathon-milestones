b731e00b9353f1a03582b16d25209b21199d29c4
Zunairagitupinit <161698417+Zunairagitupinit@users.noreply.github.com>
2024-11-10 19:40:53 +0500

    Add files via upload

---
+++ Milestone 1/script.js
@@ -0,0 +1,10 @@
+var togglesButton = document.getElementById('toggle-skills');
+var skills = document.getElementById('skills');
+togglesButton.addEventListener('click', function () {
+    if (skills.style.display === 'none') {
+        skills.style.display = 'block';
+    }
+    else {
+        skills.style.display = 'none';
+    }
+});
